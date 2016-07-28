11d07fa3d4cdf45a63878877b5e3723482ea13f3
Enver Bisevac <enver@bisevac.com>
2016-07-28 20:49:16 +0200

    support for query set messaging

--- fcm/models.py
+++ fcm/models.py
@@ -1,8 +1,24 @@
 import logging
+from django.db.models.query import QuerySet
 from django.db import models
 from fcm.utils import FCMMessage
 
 logger = logging.getLogger(__name__)
+
+
+class DeviceQuerySet(QuerySet):
+
+    def send_message(self, data, **kwargs):
+        if self:
+            registration_ids = list(self.values_list("reg_id", flat=True))
+            return FCMMessage().send(
+                data, registration_ids=registration_ids, **kwargs)
+
+
+class DeviceManager(models.Manager):
+
+    def get_queryset(self):
+        return DeviceQuerySet(self.model)
 
 
 class AbstractDevice(models.Model):
@@ -10,6 +26,8 @@ class AbstractDevice(models.Model):
     reg_id = models.CharField(verbose_name=("Registration ID"), max_length=255, unique=True)
     name = models.CharField(verbose_name=("Name"), max_length=255, blank=True, null=True)
     is_active = models.BooleanField(verbose_name=("Is active?"), default=False)
+
+    objects = DeviceManager()
 
     def __str__(self):
         return self.dev_id
