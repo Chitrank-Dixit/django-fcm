9e9c0ecae6e1865ac7bcca92314465b6ecdf09e1
Enver Bisevac <enver@bisevac.com>
2016-07-28 20:30:12 +0200

    Unique validation problem on serializer turned off for activation and deactivation of devices

--- fcm/views.py
+++ fcm/views.py
@@ -8,13 +8,20 @@ class DeviceViewSet(viewsets.ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=False)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def perform_create(self, serializer):
         try:
-            device = Device.objects.get(dev_id=serializer.validated_data["dev_id"])
+            device = Device.objects.get(dev_id=serializer.data["dev_id"])
         except Device.DoesNotExist:
-            device = Device(dev_id=serializer.validated_data["dev_id"])
+            device = Device(dev_id=serializer.data["dev_id"])
         device.is_active = True
-        device.reg_id = serializer.validated_data["reg_id"]
+        device.reg_id = serializer.data["reg_id"]
         device.save()
 
     def destroy(self, request, *args, **kwargs):
