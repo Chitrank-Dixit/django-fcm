8ac84cff2f08504642869945243e9592437f7b88
Chitrank-Dixit <chitrankdixit@gmail.com>
2016-11-10 10:22:02 +0530

    update new version of the fcm

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-fcm',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     description='A Django package that enables sending messages using FCM (Firebase Cloud Messaging).',
