54620019e7735f62ed9701d0b5a508dea643463a
Chitrank-Dixit <chitrankdixit@gmail.com>
2017-01-24 13:08:43 +0530

    added unit and functional tests in django-fcm

---
+++ fcm/tests/test_unit.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from fcm.models import Device
+
+
+class DeviceTest(TestCase):
+    def setUp(self):
+        pass
+
+    def create_device(self):
+        pass
+
+
+
+class
+
+
