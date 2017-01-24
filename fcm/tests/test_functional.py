13e803ba6a1703b200accfe36803832857b767a5
Chitrank-Dixit <chitrankdixit@gmail.com>
2017-01-24 13:09:06 +0530

    add tests

--- fcm/tests/test_functional.py
+++ fcm/tests/test_functional.py
@@ -1,1 +1,17 @@
-__author__ = 'chitrankdixit'
+from rest_framework.test import APITestCase
+
+class CreateUpdateDevice(APITestCase):
+    def setUp(self):
+        pass
+
+    def create_device(self):
+        pass
+
+    def get_device(self):
+        pass
+
+    def update_device(self):
+        pass
+
+    def delete_device(self):
+        pass
--- fcm/tests/test_unit.py
+++ fcm/tests/test_unit.py
@@ -4,13 +4,23 @@ from fcm.models import Device
 
 class DeviceTest(TestCase):
     def setUp(self):
-        pass
+        self.dev_id = "12345"
+        self.reg_id = "12345"
+        self.name = "fcm_test_device"
+        self.is_active = True
+
 
     def create_device(self):
-        pass
+        self.device = Device.objects.create(
+            dev_id = self.dev_id,
+            reg_id = self.reg_id,
+            name = self.name,
+            is_active = self.is_active
+        )
 
+        #self.assertEqual(self.device.id, self.tracking_source_name)
 
-
-class
+    def send_message(self):
+        self.device.send_message()
 
 
